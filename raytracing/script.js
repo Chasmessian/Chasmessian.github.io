bcbb53aba983e141a349128a13d021b268b2be69
Chasmessian <91864786+Chasmessian@users.noreply.github.com>
2022-04-15 11:24:42 -0500

    Create script.js

---
+++ raytracing/script.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+var walls=[[0,20,20,10],[0,0,300,200],[200,300,400,100],[0,500,200,400],[300,450,500,500], [400,250,500,300]]
+let mouseX;
+let mouseY;
+const rayLength = 200;
+const rayAmount = 1440;
+const rayInterval = 360/rayAmount;
+
+let mainLoop = setInterval(function(){
+    Mainrender();
+},16)
+
+
+
+function Mainrender(){
+    ctx.clearRect(0,0,500,500);
+    renderRays();
+
+    renderwalls();
+}
+
+function renderwalls(){
+    for(var i = 0; i<walls.length; i++){
+       ctx.beginPath();
+       ctx.moveTo(walls[i][0],walls[i][1]);
+       ctx.lineTo(walls[i][2],walls[i][3])
+       ctx.strokeStyle = "#000000";
+       ctx.stroke();
+    }
+}
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    mouseX = evt.clientX - rect.left;
+    mouseY = evt.clientY - rect.top;
+}
+
+function renderRays(){
+for(var i = 0; i<rayAmount; i++){
+ctx.beginPath()
+ctx.moveTo(mouseX,mouseY)
+let endPoints = new ray(i * rayInterval).getEndPoints()
+ctx.lineTo(endPoints[0],endPoints[1])
+ctx.strokeStyle = "#0000FF"
+ctx.stroke()
+}
+//make rays invisible, draw a line between the current endpoint and the next ray
+
+}
+
+function getDistance(x1,y1,x2,y2){
+return(Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2)))
+}
