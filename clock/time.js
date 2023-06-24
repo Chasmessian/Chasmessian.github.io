cf2c0e486bdd91e1c03003eeab193003b4693cf5
Chasmessian <91864786+Chasmessian@users.noreply.github.com>
2023-06-24 14:14:26 -0500

    Create time.js

---
+++ clock/time.js
@@ -0,0 +1,23 @@
+timer = document.getElementById("clock")
+i = 0
+loop = setInterval(function(){
+    i++
+    now = new Date()
+    
+    normal = {
+        hours: now.getHours(),
+        minutes:now.getMinutes(),
+        seconds:now.getSeconds(),
+        totalSeconds: now.getMinutes() * 60 + now.getSeconds()
+    }
+    end = "AM"
+    newHours = normal.hours
+    if(normal.hours>12){
+        newHours-=12
+        end = "PM"
+    }
+    newMinutes = Math.floor(normal.totalSeconds/40)
+    newSeconds = normal.totalSeconds - newMinutes * 40
+    string = normal.hours + ":" + newMinutes + ":" + newSeconds + " " + end
+    timer.innerHTML = string
+},1000)
