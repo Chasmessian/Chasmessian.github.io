b0099cf24cb50507498342f8b8f459c91b26a3d7
Chasmessian <91864786+Chasmessian@users.noreply.github.com>
2024-04-25 22:22:08 -0500

    Update music.js

--- music/music.js
+++ music/music.js
@@ -1,3 +1,4 @@
+begin = alert("Press ok when ready to begin")
 notes = [[27, "pencil scratches"], [43, "cough"], [59, "pencil scratches"], [69, "frustrated sigh"], [85, "pencil scratches"], [95,"water bottle falls over"], [105, "pencil scratches"], [111, "1 person walks in and stands in back"], [127, "pencil scratches, more people enter"], [137, "everyone enters, starts pacing"], [147, "pencil scratches, everyone moves closer"], [154, "1 person : 'do you really think you can do this?'"], [164, "more people start quiet, sparse comments of 'you are going to fail', more pencil scratches"], [170, "all - slightly louder, 'you are bad at theory', 'you're bad at school', 'you can't do this'"], [176, "all loudly torment, 'you are an idiot', 'you can't do anything right', 'why are you trying?', 'just give up'"], [180, "paper rips"]]
 time = 0
 document.getElementById("event").innerHTML = notes[0][1]
