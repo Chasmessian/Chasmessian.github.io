0e6fb4bfa4a80c2ff3d7895d2cb9a11166449fd8
Evan Larson <91864786+Chasmessian@users.noreply.github.com>
2024-08-30 21:24:51 -0500

    Create sober.js

---
+++ tuition/sober.js
@@ -0,0 +1,15 @@
+let semesterPrice = 6222
+let off = 8
+let workDays = 80
+let days = 121
+let SecondsInDay = 60*60*24
+let rate = (semesterPrice/(days-off))/(SecondsInDay)
+let total = 0
+let elapsedStartTime = (Date.now() - new Date(2024,7,14).getTime())/1000
+total = elapsedStartTime * rate
+// console.log(elapsedStartTime * rate)
+setInterval(function(){
+    total += rate * (16/1000)
+    // console.log(total)
+    document.getElementById("tuition").innerHTML = "$" + total
+},16)
