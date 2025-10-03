21d4d1a589a7e38f077676c33ba72a4c0d07315d
Evan Larson <91864786+Chasmessian@users.noreply.github.com>
2025-10-03 16:00:35 -0500

    Create memory.js

---
+++ memory/memory.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Memory</title>
+    <script src="memory.js"></script>
+</head>
+<body>
+    <h1>Suit Pair:</h1>
+    <p id="suitpair", style="font-size:1000%">[suit here]</p>
+    <button onclick="generateRandom()", style="width: 200px; height: 200px;"></button>Create New</button>
+</body>
+</html>
