66d1a6576c3f6ea98a6471507353b0d80a90b052
Evan Larson <91864786+Chasmessian@users.noreply.github.com>
2025-10-03 16:06:26 -0500

    Update memory.js

--- memory/memory.js
+++ memory/memory.js
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Memory</title>
-    <script src="memory.js"></script>
-</head>
-<body>
-    <h1>Suit Pair:</h1>
-    <p id="suitpair", style="font-size:1000%">[suit here]</p>
-    <button onclick="generateRandom()", style="width: 200px; height: 200px;"></button>Create New</button>
-</body>
-</html>
+var suits = ["♠️", "♣️", "♥️", "♦️"]
+function generateRandom(){
+    var i = Math.floor(Math.random()*4)
+    var j = Math.floor(Math.random()*4)
+    string = "" + suits[i] + suits[j]
+    document.getElementById("suitpair").innerHTML = string
+}
