2555a3b125879ef50a8c8c33c268e87eb47fb9ad
Evan Larson <91864786+Chasmessian@users.noreply.github.com>
2025-10-03 16:12:01 -0500

    Delete memory/memory.js

--- memory/memory.js
+++
@@ -1,7 +0,0 @@
-var suits = ["♠️", "♣️", "♥️", "♦️"]
-function generateRandom(){
-    var i = Math.floor(Math.random()*4)
-    var j = Math.floor(Math.random()*4)
-    string = "" + suits[i] + suits[j]
-    document.getElementById("suitpair").innerHTML = string
-}
